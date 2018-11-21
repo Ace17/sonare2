f023e1f7bd83b7fb539b295454baaa195a0bd658
Sebastien Alaiwan <sebastien.alaiwan@gmail.com>
2018-11-21 22:09:25 +0100

    Homogenize and bump copyright notices

--- core/main.d
+++ core/main.d
@@ -1,11 +1,9 @@
-/*
- * Copyright (C) 2017 - Sebastien Alaiwan
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as
- * published by the Free Software Foundation, either version 3 of the
- * License, or (at your option) any later version.
- */
+// Copyright (C) 2018 - Sebastien Alaiwan
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
 
 // Entry point and main loop.
 // Ideally, shouldn't know about any concrete view or machine architecture.
--- core/view.d
+++ core/view.d
@@ -1,11 +1,9 @@
-/*
- * Copyright (C) 2017 - Sebastien Alaiwan
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as
- * published by the Free Software Foundation, either version 3 of the
- * License, or (at your option) any later version.
- */
+// Copyright (C) 2018 - Sebastien Alaiwan
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
 
 // Representation of the screen state, in an I/O agnostic way.
 // Should be "ready-to-render" by an (almost) passive view.
